228470092337a538d4620877a766624a2b0ad0bf
Sophieproject <petya.antal@gmail.com>
2023-12-14 12:55:59 +0100

    almost done

--- example.py
+++ example.py
@@ -1,3 +1,33 @@
-import random
-print("Frequency of numbers in an interval: ")
-print("Mindjárt kicsöngetnek, takarodjuk haza!")
+
+def randomGenerator(s, e, a):
+    import random
+    numbers = []
+    for i in range(a):
+       
+        numbers.append(random.randint(s, e))
+    return numbers
+
+#felhasználó által megadott intervallumban generálni felhasználó álltal megadott számot, hány tökéletes szám van az intervallumban osztói összege: szám
+def makeNumber(text):
+    #isCorrect = False
+    while True: #not isCorrect:
+        n = input(text)
+        try:
+            n= int(n)
+            #isCorrect = True
+            return n
+        except ValueError:
+            print("Unexpected value")
+
+###############################################
+startMessage = "Startvalue: "
+
+endMessage = "Endvalue: "
+amountInput = "Number of numbers: "
+start = makeNumber(startMessage) 
+stop = makeNumber(endMessage)
+amount = makeNumber(amountInput)
+
+
+print(randomGenerator(start,stop,amount))
+
